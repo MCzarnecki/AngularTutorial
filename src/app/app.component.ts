ad64bae393978efaf1c9390b011cbb7a1ed24c10
Beata <beatka1102@wp.pl>
2017-06-04 23:54:54 +0200

    first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  title = 'app works!';
+  name: string = "Beti";
+  name2: string = "Michal";
+  n : string;
 
 }
